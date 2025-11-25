bbe87323fb9beda0978eaba649bdc38cda76099c
Chelovek2343 <79435067+Chelovek2343@users.noreply.github.com>
2025-11-25 20:07:12 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,99 @@
+let navbar = document.querySelector('.header .navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+};
+
+/* let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+registerBtn.onclick = () =>{
+  registerBtn.classList.add('active');
+  loginBtn.classList.remove('active');
+  document.querySelector('.account-form .login-form').classList.remove('active');
+  document.querySelector('.account-form .register-form').classList.add('active');
+};
+
+loginBtn.onclick = () =>{
+  registerBtn.classList.remove('active');
+  loginBtn.classList.add('active');
+  document.querySelector('.account-form .login-form').classList.add('active');
+  document.querySelector('.account-form .register-form').classList.remove('active');
+};
+
+let accountForm = document.querySelector('.account-form')
+
+document.querySelector('#account-btn').onclick = () =>{
+  accountForm.classList.add('active');
+}
+ 
+document.querySelector('#close-form').onclick = () =>{
+  accountForm.classList.remove('active');
+};*/
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+  acco.onclick = () =>{
+    accordion.forEach(dion => dion.classList.remove('active'));
+    acco.classList.toggle('active');
+  };
+});
+
+document.querySelector('.btn').onclick = () =>{
+  document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
+    show.style.display = 'block';
+  });
+  document.querySelector('.btn').style.display = 'none';
+};
+
+
+
+
+(function () {
+  "use strict";
+
+  var cookieAlert = document.querySelector(".cookie-alert");
+  var acceptCookies = document.querySelector(".accept-cookies");
+
+  if (cookieAlert != undefined) {
+    cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
+
+    if (!getCookie("acceptCookies")) {
+      cookieAlert.classList.add("show");
+    }
+
+    acceptCookies.addEventListener("click", function () {
+      setCookie("acceptCookies", true, 60);
+      cookieAlert.classList.remove("show");
+    });
+  }
+})();
+
+// Cookie functions stolen from w3schools
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
