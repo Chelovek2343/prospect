52eb9d5d687f929b95a61d4c9f06a3505dd23c34
Ulugbek Osmonov <agusakurdak@gmail.com>
2025-12-30 21:40:23 +0600

    Added a new slider, and fixed the mobile version

--- js/script.js
+++ js/script.js
@@ -31,3 +31,9 @@ var swiper = new Swiper('.mySwiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+var swiper = new Swiper('.reviewSwiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
